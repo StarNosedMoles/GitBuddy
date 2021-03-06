43ff7a3a05539fde8fbd84a0bc17f7ac4b294031
David Bernstein <dxbernstein@gmail.com>
2021-03-06 17:52:45 -0500

    checkboxes and tobesent map created, fetch request to mongo built

--- client/components/DataDisplay.jsx
+++ client/components/DataDisplay.jsx
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 
 
 function DataDisplay(props){
-  console.log('props: ', props.checkedItems)
-  const repoObj = props.checkedItems
-  console.log('repoObj:', repoObj)
+  // console.log('props: ', props.checkedItems)
+  const repoObj = props.toBeSent
+  // console.log('repoObj:', repoObj)
   const repoArray = [];
   for(const [key, value] of repoObj){
-    console.log("prop in loop", key)
+    // console.log("prop in loop", key)
     if (value){
       repoArray.push(<p>{key}</p>)
 
     }
   }
 
-  console.log('repoArray: ', repoArray)
+  // console.log('repoArray: ', repoArray)
   // const items = []
   // props.fakeCheckedItems.forEach(el=>{
   //   items.push(<p>{el}</p>)
