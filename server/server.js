8d5e201e355d3eca2bd546faf804208ae0fa3f79
Kushal Talele <kushal.talele@gmail.com>
2021-03-05 22:56:21 -0500

    more backend

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const path = require('path');
 
 //auth controllers
 const OAuthController = require('./controllers/auth/OAuthController');
 const cookieController = require('./controllers/auth/cookieController');
-
+const sessionController = require('./controllers/auth/sessionController');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   return res
     .status(200)
-    .sendFile(path.resolve(__dirname, "../client/index.html"));
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
-<<<<<<< HEAD
-app.use('/build', express.static(path.join(__dirname, '../build')))
-=======
 app.use('/build', express.static(path.join(__dirname, '../build')));
 //main OAuth Complete
 app.get('/login', 
   OAuthController.getCode,
   cookieController.setSSIDCookie,
+  sessionController.startSession,
   //startSession
   
   (req, res) => {
@@ -33,7 +31,6 @@ app.get('/login',
     return res.send('OAuth login complete');
   }
 );
->>>>>>> 50d00c9fc1a951b5f2e4c11102d08299b1667c60
 
 app.use('*', (req,res) => {
   res.status(404).send('Not Found');
@@ -41,16 +38,16 @@ app.use('*', (req,res) => {
    
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send("Internal Server Error");
+  res.status(500).send('Internal Server Error');
 });
 
-if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV === 'production'){
 // statically serve everything in the build folder on the route '/build'
-app.use('/build', express.static(path.join(__dirname, '../build')));
-// serve index.html on the route '/'
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+  });
 }
   
 app.listen(3000);
