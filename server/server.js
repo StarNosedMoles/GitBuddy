1eb119c3fe8f0f1efba0959d821d58c5ac81888a
Joseph Stern <josephiswhere@gmail.com>
2021-03-05 20:15:47 -0500

    pool finished

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, "../client/index.html"));
 });
 
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
-
-app.use('*', (req,res) => {
-  res.status(404).send('Not Found');
+app.use("*", (req, res) => {
+  res.status(404).send("Not Found");
 });
-  
-  
+
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send('Internal Server Error');
+  res.status(500).send("Internal Server Error");
 });
-  
-  
+
 app.listen(3000);
