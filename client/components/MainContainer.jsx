575cd77c75307de703c68e206891492bb48c7807
Gregory Levine-Rozenvayn <grisha617@gmail.com>
2021-03-08 10:54:34 -0500

    css changes, nothing major just messing around

--- client/components/MainContainer.jsx
+++ client/components/MainContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import RepoGrabber from './RepoGrabber.jsx';
-import DataDisplay from './DataDisplay';
+import DataDisplay from './DataDisplay'
 
 class MainContainer extends Component {
   constructor(props){
@@ -10,12 +10,9 @@ class MainContainer extends Component {
       name: '',
       // repos: [{name: "myFirst Repo", followers: [1,2,3,4,5]},
       repos: [],
-      personalFollowers: [{ name : 'Kushal', email: 'fakeemail@email.email' },  
-      { name : 'David', email: 'fakeemail@email.email' },  
-      { name : 'Joseph', email: 'fakeemail@email.email' },  
-      { name : 'Greg', email: 'fakeemail@email.email' },],
+      personalFollowers: [],
       checked: new Map(),
-      toBeSent: [],
+      toBeSent: new Map(),
     };
     this.getFollowers = this.getFollowers.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -23,35 +20,22 @@ class MainContainer extends Component {
   }
 
   getFollowers(){
-    let toBeSent = []
+    let toBeSent = new Map();
     for(const [key, value] of this.state.checked){
+      console.log("key in loop", key, value)
     if (value===true){
-      for(let repo of this.state.repos){
-        if(key === repo.name){toBeSent.push(repo.stargazersUrl)}
-      }
-    }
+      toBeSent.set(key, value);
+    }}
+    console.log("toBeSent in func?", toBeSent)
+    this.setState({...this.state, toBeSent: toBeSent})
+    setTimeout(() => {console.log("toBeSent in state?", this.state)}, 1000)
+    // fetch request to db for user's repo follower data should go here
   }
-  this.setState({...this.state, toBeSent});
-  console.log(toBeSent)
-  // fetch request to db for user's repo follower data should go here
-  fetch('/repoPost', {
-    method: 'POST',
-    body: toBeSent
-  })
-  .then((res) => res.json())
-  .then((data) => {
-    // this.setState({...this.state, personalFollowers: data.followers})
-    console.log(data)
-  })
-  .catch(err => console.log(err))
-}
-    
 
     handleChange(e) {
     const item = e.target.name;
     const isChecked = e.target.checked;
-    this.setState(prevState => ({...this.state, checked: prevState.checked.set(item, isChecked) }))
-    // console.log(this.state)
+    this.setState(prevState => ({...this.state, checked: prevState.checked.set(item, isChecked) }));
   }
 
   componentDidMount () {
@@ -69,7 +53,8 @@ class MainContainer extends Component {
   render(){
     return(
       <div className="MainContainer">
-        <h3 className="greeting">Hi, {this.state.name}. Would you like to see your bomb ass repos?</h3>
+        <h3 className="greeting">Hi, {this.state.name}.</h3>
+        <p>Select your GitHub Followers and/or your Repo Stargazers</p>
         <RepoGrabber 
           repos={this.state.repos}
           personalFollowers={this.state.personalFollowers}
